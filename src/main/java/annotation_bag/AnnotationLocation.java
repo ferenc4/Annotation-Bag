087fce96225044cfaebb57cc32c36624cfe839ac
Ferenc <ferenc4.fazekas@gmail.com>
2017-02-25 12:36:33 +1100

    removed unused class

--- src/main/java/annotation_bag/AnnotationLocation.java
+++
@@ -1,9 +0,0 @@
-package annotation_bag;
-
-import annotation_bag.annotations.author;
-import annotation_bag.annotations.date;
-
-@author("Ferenc Fazekas")
-@date("2/25/2017")
-public enum AnnotationLocation {
-}
