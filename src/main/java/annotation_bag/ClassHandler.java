6739515dff9b0cd34310d42aa6cb7911ea16d27c
Ferenc <ferenc4.fazekas@gmail.com>
2017-02-28 23:01:40 +1100

    include java file that wasn't compiled nor loaded

--- src/main/java/annotation_bag/ClassHandler.java
+++ src/main/java/annotation_bag/ClassHandler.java
@@ -3,7 +3,12 @@ package annotation_bag;
 import annotation_bag.annotations.author;
 import annotation_bag.annotations.date;
 
+import javax.tools.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,5 +27,19 @@ public class ClassHandler {
         String packagePath = packageFilePath.substring(0, packageFilePath.length() - suffix.length());
         packagePath = packagePath.replaceAll("\\\\|/", ".");
         return Class.forName(packagePath);
+    }
+
+    static Class<?> includeSrcFile(File sourceFile, String classname) throws IOException, ClassNotFoundException {
+        // compile the source file
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+        File parentDirectory = sourceFile.getParentFile();
+        fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(parentDirectory));
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(sourceFile));
+        compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
+        fileManager.close();
+        // load the compiled class
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{parentDirectory.toURI().toURL()});
+        return classLoader.loadClass(classname);
     }
 }
--- src/test/java/annotation_bag/ClassHandlerTest.java
+++ src/test/java/annotation_bag/ClassHandlerTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import testdata.codebase.AnnotatedByPurpose;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Method;
 
 @author("Ferenc Fazekas")
 @date("2/26/2017")
@@ -39,6 +41,26 @@ public class ClassHandlerTest {
     public void rootPackageNameMissingFromPath() throws Exception {
         File classFile = new File("src/test/java/testdata/codebase/AnnotatedByPurpose.java");
         ClassHandler.getClassOnClassPathFromJavaFile(classFile, "bad");
+    }
+
+    @Test
+    public void includeSrcFile() throws Exception {
+        // create an empty source file
+        File sourceFile = File.createTempFile("Hello", ".java");
+        sourceFile.deleteOnExit();
+        // generate the source code, using the source filename as the class name
+        String classname = sourceFile.getName().split("\\.")[0];
+        String sourceCode = "public class " + classname + "{ public String helloStr() { return \"Hello world!\";}}";
+        // write the source code into the source file
+        FileWriter writer = new FileWriter(sourceFile);
+        writer.write(sourceCode);
+        writer.close();
+        Class<?> helloClass = ClassHandler.includeSrcFile(sourceFile, classname);
+
+        // call a method on the loaded class
+        Method helloMethod = helloClass.getDeclaredMethod("helloStr");
+        String helloStrResult = String.valueOf(helloMethod.invoke(helloClass.newInstance()));
+        Assertions.assertThat(helloStrResult).isEqualTo("Hello world!");
     }
 
     @Before
